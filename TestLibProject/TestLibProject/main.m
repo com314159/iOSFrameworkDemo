7380f54ad6906ee8ac93469e8283d33995b08268
guozhicheng <com314159@qq.com>
2016-01-21 16:24:30 +0800

    先提交一个测试测试喽

---
+++ TestLibProject/TestLibProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestLibProject
+//
+//  Created by guozhicheng on 1/21/16.
+//  Copyright © 2016 guozhicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KSAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KSAppDelegate class]));
+    }
+}
