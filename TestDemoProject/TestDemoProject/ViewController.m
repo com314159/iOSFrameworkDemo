3810f402ca2134a75c839d63d17fa1cc0db6bf09
guozhicheng <com314159@qq.com>
2016-01-21 19:30:38 +0800

    先提交一次

--- TestDemoProject/TestDemoProject/ViewController.m
+++ TestDemoProject/TestDemoProject/ViewController.m
@@ -10,13 +10,30 @@
 
 @interface ViewController ()
 
+@property (nonatomic,strong) UIButton *testButton;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.testButton = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 100, 80)];
+    self.testButton.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.testButton];
+    
+    
+    [self.testButton addTarget:self action:@selector(testFunc) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+- (void) testFunc {
+    
+    NSLog(@"testFunc");
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
